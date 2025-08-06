cae37eae35571365f00431ad9ce3ac84e535bcfa
DEV-thinktrend <dev.thinktrend@yahoo.com>
2025-08-06 23:59:51 +0100

    'user'

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const User = sequelize.define('User', {
-  username: { type: DataTypes.STRING, unique: true },
+  username: { type: DataTypes.STRING },
   password: { type: DataTypes.STRING },
   role: { type: DataTypes.ENUM('admin', 'sub-admin'), defaultValue: 'sub-admin' },
   approved: { type: DataTypes.BOOLEAN, defaultValue: false },
